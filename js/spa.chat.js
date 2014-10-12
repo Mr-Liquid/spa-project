19d7d5b41a66b81ec93bda46d82391424986590c
Ivan <ivan.webgroup@gmail.com>
2014-10-12 14:02:29 +0300

    added example files

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -24,23 +24,82 @@ spa.chat = (function(){  // пространство имен для модул�
   var
 	configMap = {
 	  main_html : String()
-		+ '<div style="padding:1em; color:#fff;">'
-		+ 'Say hello to chat'
+		+ '<div class="spa-chat">'
+		+ '<div class="spa-chat-head">'
+		+ '<div class="spa-chat-head-toggle">+</div>'
+		+ '<div class="spa-chat-head-title">'
+		+ 'Chat'
+		+ '</div>'
+		+ '</div>'
+		+ '<div class="spa-chat-closer">x</div>'
+		+ '<div class="spa-chat-sizer">'
+		+ '<div class="spa-chat-msgs"></div>'
+		+ '<div class="spa-chat-box">'
+		+ '<input type="text"/>'
+		+ '<div>send</div>'
+		+ '</div>'
+		+ '</div>'
 		+ '</div>',
-	  settable_map : {}
+	  settable_map : {
+		slider_open_time    : true,
+		slider_close_time   : true,
+		slider_opened_em    : true,
+		slider_closed_em    : true,
+		slider_opened_title : true,
+		slider_closed_title : true,
+
+		chat_model      : true,
+		people_model    : true,
+		set_chat_anchor : true
+	  },
+	  slider_open_time     : 250,
+	  slider_close_time    : 250,
+	  slider_opened_em     : 18,
+	  slider_closed_em     : 2,
+	  slider_opened_title  : 'Click to close',
+	  slider_closed_title  : 'Click to open',
+	  slider_opened_min_em : 10,
+	  window_height_min_em : 20,
+
+	  chat_model      : null,
+	  people_model    : null,
+	  set_chat_anchor : null
 	},
-	stateMap  = { $container : null },
+	stateMap  = {
+	  $append_target   : null,
+	  position_type    : 'closed',
+	  px_per_em        : 0,
+	  slider_hidden_px : 0,
+	  slider_closed_px : 0,
+	  slider_opened_px : 0
+	},
 	jqueryMap = {},
-	setJqueryMap, configModule, initModule;
+	setJqueryMap, configModule, initModule, getEmSize, setPxSizes, setSliderPosition, onClockToggle;
+
+  	getEmSize = function ( elem ) { // метод для преобразования em в px, чтобы можно было производить измерения в jquery
+	  return Number(
+		ggetComputedStyle( elem, '' ).fontSize.match(/\d*\.?\d*/)[0]
+	  );
+	};
 
   // end var define
 
   //начало метода DOM /setJqueryMap/
 
-  setJqueryMap = function () {
-	var $container = stateMap.$container;
-	jqueryMap = { $container: $container };
-  }
+  var
+	$append_target = stateMap.$append_target,
+	$slider = $append_target.find( '.spa-chat');
+
+  jqueryMap = {
+	$slider : $slider,
+	$head   : $slider.find( '.spa-chat-head' ),
+	$toggle : $slider.find( '.spa-chat-head-toggle' ),
+	$title  : $slider.find( '.spa-chat-head-title' ),
+	$sizer  : $slider.find( '.spa-chat-sizer' ),
+	$msgs   : $slider.find( '.spa-chat-msgs' ),
+	$box    : $slider.find( '.spa-chat-box' ),
+	$input  : $slider.find( '.spa-chat-input input[type=text]')
+  };
 
   //конец метода DOM
 
