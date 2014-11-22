40321dec9dbc4bedc789217604cd3777434eea2f
Ivan <ivan.webgroup@gmail.com>
2014-11-22 14:54:43 +0300

    modified model.js and shell.js

--- js/spa.model.js
+++ js/spa.model.js
@@ -23,11 +23,12 @@ spa.model = (function (){
           cid_serial     : 0,
           people_cid_map : {},
           people_db      : TAFFY(),
-          user           : null
+          user           : null,
+          is_connected   : false // определяет находится ли пользователь в чате
       },
 	isFakeData = true, // говорит модели использовать fake данные а не настоящие
       personProto, makeCid, clearPeopleDb, completeLogin,
-      makePerson, removePerson, people, initModule;
+      makePerson, removePerson, people, initModule, chat;
     // The people object API
     // ---------------------
     // The people object is available at spa.model.people.
@@ -124,6 +125,19 @@ spa.model = (function (){
         stateMap.people_db.insert( person );
         return person;
     };
+    removePerson = function ( person ) {
+        if ( ! person ) { return false; }
+        // cannot remove anonymous person
+        if ( person.id === configMap.anon_id ) {
+            return false;
+        }
+
+        stateMap.people_db({ cid : person.cid }).remove();
+        if ( person.cid ) {
+            delete stateMap.people_cid_map[ person.cid ];
+        }
+        return true;
+    };
 
   people = (function(){ //замыкание people позваляет показывать методы которые хотим
       var get_by_cid, get_db, get_user, login, logout;
@@ -172,6 +186,75 @@ spa.model = (function (){
       };
 
   })();
+    chat = (function(){
+      var
+        _publish_listchange,
+        _update_list, _leave_chat, join_chat;
+
+        //внутренние методы
+
+        _update_list = function( arg_list){ //обновляет объект people послк получения нового списка людей.
+          var i, person_map, make_person_map,
+              people_list = arg_list[ 0 ];
+        clearPeopleDb();
+
+        PERSON:
+        for( i = 0; i < people_list.length; i++){
+          person_map = people_list[ i ];
+
+          if(!person_map.name){ continue PERSON;}
+          //если пользователь определенб обновить css_map и ольше ничего не делать
+          if(stateMap.user && stateMap.user.id === person_map._id){
+             stateMap.user.css_map = person_map.css_map;
+             continue PERSON;
+          }
+          make_person_map = {
+              cid: person_map._id,
+              css_map: person_map.css_map,
+              id: person_map._id,
+              name: person_map.name
+          };
+
+          makePerson( make_person_map );
+        }
+            stateMap.people_db.sort('name');
+        };
+
+        _publish_listchange = function( arg_list ){ //публикует глобальное пользовательское событие spa-listchange сопровождаемое обновленным списком людей
+            _update_list(arg_list);
+            $.gevent.publish('spa-listchange',[arg_list]);
+        };
+
+        //конец внутренних методов
+
+        _leave_chat = function(){ // отправляет серверу сообщение leavechat и очищает переменные состояния
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+            stateMap.is_connected = false;
+            if( sio ) { sio.emit( 'leavechat') }
+        }
+
+        join_chat = function(){ //вызывается, чтобы войти в чат. Он проверяет находится ли уже пользователь в сети stateMap.is_connected чтобы не регистрировать обрабочтик события listchange более одного раза
+            var sio;
+
+            if(stateMap.is_connected){
+                return false;
+            }
+            if( stateMap.user.get_is_anon()){
+                console.log('User must be defined befor joining chat');
+                return false;
+            }
+
+            sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            sio.on('listchange', _publish_listchange);
+            return true;
+        }
+
+        return {
+            _leave: _leave_chat,
+            join: join_chat
+        }
+    })();
   initModule = function(){
 	var i, people_list, person_map;
 	// инициализируем анонимного пользователя
@@ -182,21 +265,22 @@ spa.model = (function (){
     });
     stateMap.user = stateMap.anon_user;
 
-      if ( isFakeData ) {
-          people_list = spa.fake.getPeopleList();
-          for ( i = 0; i < people_list.length; i++ ) {
-              person_map = people_list[ i ];
-              makePerson({
-                  cid     : person_map._id,
-                  css_map : person_map.css_map,
-                  id      : person_map._id,
-                  name    : person_map.name
-              });
-          }
-      }
+//      if ( isFakeData ) {
+//          people_list = spa.fake.getPeopleList();
+//          for ( i = 0; i < people_list.length; i++ ) {
+//              person_map = people_list[ i ];
+//              makePerson({
+//                  cid     : person_map._id,
+//                  css_map : person_map.css_map,
+//                  id      : person_map._id,
+//                  name    : person_map.name
+//              });
+//          }
+//      }
   };
   return {
     initModule : initModule,
-    people : people
+    people : people,
+    chat: chat
   };
 }());
